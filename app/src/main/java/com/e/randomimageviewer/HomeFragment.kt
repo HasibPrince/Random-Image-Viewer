2242b22d78a209a76837ededcfe52d8ab9d683e2
HasibPrince <hasibprince314@gmail.com>
2021-11-20 23:37:26 +0600

    random image generated on button click

--- app/src/main/java/com/e/randomimageviewer/HomeFragment.kt
+++ app/src/main/java/com/e/randomimageviewer/HomeFragment.kt
@@ -6,8 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.signature.ObjectKey
 import com.e.randomimageviewer.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.io.File
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
@@ -25,5 +29,12 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.randomImageButton.setOnClickListener {
+            Glide.with(this)
+                .load("https://picsum.photos/200/300")
+                .signature(ObjectKey(System.currentTimeMillis().toString()))
+                .into(binding.randomImage)
+        }
+
     }
 }
